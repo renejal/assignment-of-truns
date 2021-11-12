dc100eafefb965dab20add6a23599e72db88c891
danielferSomos <danielfer257@gmail.com>
2021-11-12 09:01:49 -0500

    json cambios y clase site introducida

--- dominio/Site.py
+++ dominio/Site.py
@@ -1,3 +1,9 @@
+from typing import List
+
+
 class Site:
-    def __init__(self, site_id : int , )->None:
-        
+    __site_id: int
+    minimum_shift_hours_if_is_special_site: int 
+
+    def __init__(self, site_id : int , minimum_shift_hours_if_is_special_site : int, working_day_start_time : int , weeks_schedule : List[any] ) -> None:
+        self.__site_id = site_id
