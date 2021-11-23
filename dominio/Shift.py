bfc3863a6152e0ebb03d4b5d22cbcf7163732060
unknown <danielfer@unicauca.edu.co>
2021-11-22 21:56:41 -0500

    cambios que no funcionan

--- dominio/Shift.py
+++ dominio/Shift.py
@@ -1,11 +1,11 @@
 from typing import List
-from dominio.problem import Problem
+from dominio.Vigilant import Vigilant
 
 class Shift:
     __shift_start: int
     __shift_end: int
     __necesary_vigilants: int 
-    __assigment_vigilants: List[Problem]
+    __assigment_vigilants: List[Vigilant]
 
     def __init__(self,shift_start: int, shift_end: int, necesary_vigilants: int) -> None:
         self.__shift_start = shift_start
