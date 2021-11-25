38fe8d4048ac95805834c58063458539c6cf3966
unknown <danielfer@unicauca.edu.co>
2021-11-24 22:05:36 -0500

    Solucion restructuracion, se añadieron y dividieron responsabilidades en servicios

--- dominio/vigilant_assigment.py
+++ dominio/vigilant_assigment.py
@@ -1,99 +1,54 @@
-from typing import List
+from typing import Dict, List
 from data.SiteDataFile import SiteDataFile
 from data.VigilantsDataFile import VigilantsDataFile
+from dominio.Shift import Shift
 from dominio.Site import Site
 from dominio.Vigilant import Vigilant
 import operator
 
 class VigilantAssigment:
-
-    __total_vigilantes = 0
-    totalPlaces=0 
-    __total_weeks: int 
-    totalPeriods=0
+    # maxShiftDuration: int = 12
+    # minShiftDuration: int = 4
+    # minBreakDuration: int = 18
+    # maxOvertimeWorkHoursPerWeek=12
+    # maxWorkHoursPerWeek=48
+    # minWorkHoursPerWeek=40
+    # idealWorkHoursPerWeek=48
 
     __vigilants: List[Vigilant]
-    
-    maxShiftDuration: int = 12
-    minShiftDuration: int = 4
-    minBreakDuration: int = 18
-    maxOvertimeWorkHoursPerWeek=12
-    maxWorkHoursPerWeek=48
-    minWorkHoursPerWeek=40
-    idealWorkHoursPerWeek=48
-
-    periodEndByWeek = []
-    cantVigilantsByPeriod = []
-    vigilantes = []
-    cantNecesaryVigilantesforSite = {}
-    workingDay={}
-    __working_day_permanet = None
-
-    specialSites={}
-    vigilantExpectedPlaces = {}
-    vigilantsWithOutPreference = []
-    orderSitesForCantVigilantes = []
-    obj_site_data_file: SiteDataFile = None
-
-
-    def __init__(self, pathInterface, pathVigilants, weeks):
-        self.__total_weeks = weeks
-        self.obj_site_data_file = None
-        self.__workingDay = None
-        self.totalPeriods = 168*self.totalWeeks
-        self.readData(pathInterface,pathVigilants)
-        self.initProblem()
+    __sites: List[Site]
+    __total_vigilantes: int
+    __total_sites: int
+    __total_weeks: int 
+    __vigilantExpectedPlaces: Dict[int , List[int]]
+    __order_sites_by_vigilants_amount: List[int]
+        
 
     def __init__(self, vigilants : List[Vigilant], sites: List[Site] , weeks ) -> None:
         self.__vigilants = vigilants
         self.__sites = sites
         self.__total_weeks = weeks
+        # TO DO OrderSites 
+        self.initProblem()
 
-    ##Le pertenece a otra clase
-    def readData(self, pathInterface , pathVigilants):
-        self.readDataInterface(pathInterface)
-        self.readVigilantData(pathVigilants)
-    ##Le pertenece a otra clase
-
-    def readDataInterface(self,pathInterface):
-        self.obj_site_data_file: SiteDataFile = SiteDataFile(pathInterface, self.totalWeeks)
-        self.SitesData= self.obj_site_data_file.get_data_problem()
-        self.__workingDay = self.obj_site_data_file.get_working_day()
-        self.specialSites = self.obj_site_data_file.specialSites
-        self.totalPlaces = len(self.SitesData)
-    ##Le pertenece a otra clase
-
-    def readVigilantData(self, pathVigilants):
-        data = VigilantsDataFile(pathVigilants)
-        self.VigilantsData = data.vigilantsInfo
-        self.totalVigilantes = data.numberVigilants
-
-    def initProblem(self):
-        #self.identifiesWeekStartPeriod()
-        self.getCantVigilantesforSite()
-        #init vigilantes and shift default
-        for i in range(self.totalVigilantes):
-            objVigilant = Vigilant(self.VigilantsData[i][0],self.VigilantsData[i][1],self.VigilantsData[i][2],self.VigilantsData[i][3],self.totalWeeks)
-            if self.VigilantsData[i][1] != 0:
-                if (self.VigilantsData[i][1] in self.vigilantExpectedPlaces) == False:
-                    self.vigilantExpectedPlaces[self.VigilantsData[i][1]] = [objVigilant.id]
+    def initProblem(self) -> None:
+        for vigilant in self.__vigilants:
+            if vigilant.__expected_place_to_work !=0:
+                if vigilant.__expected_place_to_work in self.__vigilantExpectedPlaces:
+                    self.__vigilantExpectedPlaces[vigilant.__expected_place_to_work].apppend(vigilant.__vigilant_id)
                 else:
-                    self.vigilantExpectedPlaces[self.VigilantsData[i][1]].append(objVigilant.id)
-            self.vigilantes.append(objVigilant)
-            if len(objVigilant.shiftPreferences) == 0:
-                self.vigilantsWithOutPreference.append(objVigilant.id)
+                    self.__vigilantExpectedPlaces[vigilant.__expected_place_to_work]= vigilant.__vigilant_id
+        self.createShiftsBySite()
         self.OrderSitesForCantVigilantes()
-        self.loadWorkingDay()
-    
-    ##eliminado
-    def identifiesWeekStartPeriod(self):
-        '''
-        identify the shift the init for the week
-        '''
-        for i in range(0, self.totalWeeks):
-            self.periodEndByWeek.append((i + 1) * 168)
+
+    def createShiftsBySite() -> List[List[Shift]]:
+        workingDay = self.loadWorkingDay()
+        return None
+    def getShiftsBySite() -> List[Shift]:
+        return None
 
     def getCantVigilantesforSite(self):
+        cantNecesaryVigilantesforSite = {}
         indexSite = 1
         for place in self.SitesData:
             cantVigilantByPeriod = []
@@ -102,19 +57,23 @@ class VigilantAssigment:
                 cantVigilantByPeriod.append(cantByperiod)
                 sum = sum + cantByperiod
             self.cantVigilantsByPeriod.append(cantVigilantByPeriod)
-            self.cantNecesaryVigilantesforSite[indexSite] = sum
+            cantNecesaryVigilantesforSite[indexSite] = sum
             indexSite += 1
+        return cantNecesaryVigilantesforSite
+
+    def get_order_site_by_vigilants_amount()-> int:
+        return None
 
     def OrderSitesForCantVigilantes(self):
-        sites = self.cantNecesaryVigilantesforSite
+        sites = self.getCantVigilantesforSite()
         sites = sorted(sites.items(), key=operator.itemgetter(1), reverse=True)
         site = []
         for i in sites:
             site.append(int(i[0]))
-        self.orderSitesForCantVigilantes = site
+        self.__order_sites_by_vigilants_amount = site
 
-    def loadWorkingDay(self):
-        self.workingDay = { 1:[1],2:[2],3:[3],4:[4],5:[5],6:[6], 7:[7],8:[8],9:[9],10:[10],11:[11],12:[12],13:[7,6],14:[7,7],15:[8,7],16:[8,8],17:[8,9],20:[10,10],21:[7,7,7],22:[8,7,7],
+    def loadWorkingDay(self) -> Dict[int,List[int]]: 
+        return { 1:[1],2:[2],3:[3],4:[4],5:[5],6:[6], 7:[7],8:[8],9:[9],10:[10],11:[11],12:[12],13:[7,6],14:[7,7],15:[8,7],16:[8,8],17:[8,9],20:[10,10],21:[7,7,7],22:[8,7,7],
                       23:[8,8,7],24:[8,8,8]}
 
     def get_working_day_with_constraints(self, key, site):
@@ -128,5 +87,82 @@ class VigilantAssigment:
         return self.SitesData[siteId-1]
 
 
-    def get_workig_day(self):
-        return self.__workingDay
+
+
+    
+
+
+     #PASARLO A VIGILANT ASSIGMNET
+    def specialShifts(self, startWork, endWork, specialHours):
+        shifts = []
+        while(startWork < endWork):
+            endHour = specialHours+startWork-1
+            if(endHour+specialHours > endWork):
+                endHour = endWork
+            shifts.append([startWork, endHour])
+            startWork = endHour+1
+        return shifts
+    #PASARLO A VIGILANT ASSIGMNET
+    def getSpecialShifts(self, siteId,parSite):
+        specialhours = self.__problem.specialSites[siteId]
+        shifts = []
+        start = -1
+        working = 0
+        for i in range(0, len(parSite)):
+            if(parSite[i] != 0 and working + 1 != 24):
+                if start == -1:
+                    start = i
+                working += 1
+                if i == len(parSite)-1:
+                    shifts += self.specialShifts(start, i, specialhours)
+            elif(working > 0):
+                if working == 23:
+                    shifts += self.specialShifts(start, i, specialhours)
+                else:
+                    shifts += self.specialShifts(start, i-1, specialhours)
+                start = -1
+                working = 0
+        return shifts
+    #PASARLO A VIGILANT ASSIGMNET
+    def obtainShiftBySite(self, siteId):
+        parSite = self.__problem.getSite(siteId)
+        if siteId in self.__problem.specialSites:
+            return self.getSpecialShifts(siteId,parSite)
+        else:
+            site = np.copy(parSite)
+            working_day = []
+            init = -1
+            start = False
+            k = 0
+            for index, t in enumerate(site):
+                if t == 0 and start == False:
+                    continue
+                if t != 0:
+                    if init == -1:
+                        init = index
+                    start = True
+                    if k < 24:
+                        k += 1
+
+                if (t == 0 and start == True) or k == 24:
+                    workindaytimes = self.__problem.workingDay[k]
+                    if workindaytimes == None:
+                        print(
+                            "el numero de horas no puede establecerce a un vigilantes revice EL DATASET")
+                    start = False
+                    if workindaytimes == 9:
+                        return
+                    working_day = working_day + \
+                        (self.calculateworkinday(workindaytimes, init))
+                    init = -1
+                    k = 0
+
+            return working_day
+    #PASARLO A VIGILANT ASSIGMNET
+    def calculateworkinday(self, workinday, indexWorkingDay):
+        dayShiftS = []
+        for durationShift in workinday:
+            dayShiftS.append(
+                [indexWorkingDay, indexWorkingDay+durationShift-1])
+            indexWorkingDay += durationShift
+        return dayShiftS
---
+++ services/tweak_service.py
@@ -0,0 +1,144 @@
+class tweak_service:
+
+    @staticmethod
+    def Tweak(self, solution):
+        solution = self.tweakMissingShifts(solution, True)
+        self.__problem.maxWorkHoursPerWeek = 56
+        solution = self.tweakMissingShifts(solution, False)
+        self.__problem.maxWorkHoursPerWeek = 48
+        #solution = self.tweakVigilants(solution)
+        self.calculateFitness(solution)
+        return solution
+    
+    def tweakMissingShifts(self, solution, order):
+        solution = self.tweakMissingHoursVigilants(solution,order)
+        return solution
+
+    def tweakVigilants(self, solucion):
+        if self.is_empty(solucion.vigilantsForPlaces):
+            listSite = aleatory.getAleatory(0, len(solucion.vigilantsForPlaces)-1, 2)
+            vigilantOne = aleatory.getAleatory(0, len(solucion.vigilantsForPlaces[listSite[0]]), 1)
+            vigilantTwo = aleatory.getAleatory(0, len(solucion.vigilantsForPlaces[listSite[1]]), 1)
+            self.toExchageVigilants(listSite[0], vigilantOne[0], listSite[1], vigilantTwo[0], solucion)
+        return solucion
+
+
+    def tweakMissingHoursVigilants(self,solution,order):
+        vigilantsByHours = self.GetVigilatsByHours(solution.vigilantsSchedule)
+        vigilantsByHours = collections.OrderedDict(
+            sorted(vigilantsByHours.items(), reverse=order))
+        listTempVigilant = []
+        for indexSite in range(0, len(self.missingShiftsBySite)):
+            for shift in self.missingShiftsBySite[indexSite]:
+                listTempVigilant.clear()
+                cantNecessariVigilantsInShift = self.__problem.cantVigilantsByPeriod[indexSite][shift[0]] - len(
+                    self.sitesSchedule[indexSite][shift[0]])
+                numberIterations = cantNecessariVigilantsInShift
+                for i in range(0, numberIterations):
+                    objViglant = self.getAvaibleVigilant(
+                        shift[0], shift[1], listTempVigilant, vigilantsByHours.values())
+                    if objViglant == None:
+                        continue
+                    objViglant.setShifts(shift, indexSite+1)
+                    cantNecessariVigilantsInShift -= 1
+                    for i in range(shift[0], shift[1]+1):
+                        solution.sitesSchedule[indexSite][i].append(
+                            objViglant.id)
+                        # solution.Fitness-=10000
+                    listTempVigilant.append(objViglant)
+                if cantNecessariVigilantsInShift == 0:
+                    self.missingShiftsBySite[indexSite].remove(shift)
+                self.updateHours(shift, listTempVigilant)
+                # for vigilan in listTempVigilant:
+                #     for week in vigilan.HoursWeeks:
+                #         if week > 48:
+                #             solution.Fitness+= (week-48)*100
+        return solution
+    
+
+    # def missingShiftsFormat(self, missingShiftsBySite):
+    #     shiftsFormat = []
+    #     for missingShiftsInSite in missingShiftsBySite:
+    #         shifts = []
+    #         nHoras = 0
+    #         if missingShiftsInSite:
+    #             indexShift = 0
+    #             cantMissingShifts = len(missingShiftsInSite)
+    #             for i in range(0, cantMissingShifts-1):
+    #                 if missingShiftsInSite[i]+1 != missingShiftsInSite[i+1] or nHoras == 23 or i == cantMissingShifts-2:
+
+    #                     nHoras += 1
+    #                     shifts = shifts + \
+    #                         self.calculateworkinday(
+    #                             self.__problem.workingDay[nHoras], missingShiftsInSite[indexShift])
+    #                     indexShift += nHoras
+    #                     nHoras = 0
+    #                 else:
+    #                     nHoras += 1
+    #             shifts[len(shifts)-1][1] += 1
+    #         shiftsFormat.append(shifts)
+    #     self.missingShiftsBySite = shiftsFormat
+
+    def toExchageVigilants(self, parIdSiteOne, parIdVigilantOne, parIdSiteTwo, parIdVigilantsTwo,solucion):
+        '''
+        to Exchage vigilants between sites
+        :param parSiteOne: site one select ramdoly
+        :param parVigilantOne: vigilant one select the site one ramdoly
+        :param parSiteTwo:
+        :param parVigilantsTwo:
+        :return: None
+        '''
+        try:
+            objVigilantOne = solucion.vigilantsForPlaces.get(parIdSiteOne)[
+                parIdVigilantOne]
+            objVigilantTwo = solucion.vigilantsForPlaces.get(parIdSiteOne)[
+                parIdVigilantOne]
+            self.updateFitnees(objVigilantOne, parIdSiteOne,
+                               parIdSiteTwo, solucion)
+            self.updateFitnees(objVigilantTwo, parIdSiteTwo,
+                               parIdSiteOne, solucion)
+            temVigilanOne = solucion.vigilantsForPlaces.get(parIdSiteOne)[
+                parIdVigilantOne]
+            solucion.vigilantsForPlaces.get(parIdSiteOne)[
+                parIdVigilantOne] = solucion.vigilantsForPlaces.get(parIdSiteTwo)[parIdVigilantsTwo]
+            solucion.vigilantsForPlaces.get(
+                parIdSiteTwo)[parIdVigilantsTwo] = temVigilanOne
+        except:
+            print("exception")
+    
+    def getAleatory(self, parInit, parEnd, parNumber):
+        '''
+        generate n number aleatory between  nunber init and number end
+        :param init: number init
+        :param end: number end
+        :param number: quantity the number aleatory
+        :return: list number aleatory
+        '''
+        k = 0
+        listAleatory = []
+        while True:
+            if parNumber > k:
+                numAleatory = random.randint(parInit, parEnd)
+                if numAleatory not in listAleatory:
+                    listAleatory.append(numAleatory)
+                    k = k+1
+            else:
+                break
+        return listAleatory
+    
+    def is_empty(self, list):
+        varResult = True
+        for i in list:
+            if i is None:
+                varResult = False
+                break
+        return varResult
+
+    def GetVigilatsByHours(self, vigilants):
+        vigilantByHours = {}
+        for vigilant in vigilants:
+            if vigilant.HoursWorked in vigilantByHours:
+                vigilantByHours[vigilant.HoursWorked].append(vigilant)
+            else:
+                vigilantByHours[vigilant.HoursWorked] = [vigilant]
+        return vigilantByHours
