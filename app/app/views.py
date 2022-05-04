f1910b02d5790bff65dfa16c9ae994212387319e
=rene jalvin <beimar.jalvin@vivaair.com>
2022-05-03 21:18:12 -0500

    merge

--- app/app/views.py
+++ app/app/views.py
@@ -5,8 +5,9 @@ from rest_framework.response import Response
 
 class CSVUploadHandler(APIView):
     def get(self, request: Request) -> Response:
+        print("get")
         return Response({}, status=status.HTTP_200_OK)
 
     def post(self, request: Request) -> Response:
-        print("llego",Request.data)
+        print("post",Request.data)
         return Response({}, status=status.HTTP_201_CREATED)
