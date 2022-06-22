9d0faddf73454852bfae05ae7081253211279553
=rene jalvin <beimar.jalvin@vivaair.com>
2022-06-21 22:49:53 -0500

    validaton tweat shift

--- app/services/NSGAII/tweak_shift.py
+++ app/services/NSGAII/tweak_shift.py
@@ -1,4 +1,8 @@
+from cgi import print_arguments
+import sched
 from typing import List
+
+from pkg_resources import working_set
 from dominio.model.shift import Shift
 from utils import aleatory
 from utils import union
@@ -20,7 +24,28 @@ class TweakShift:
         component_b.order_workings_days()
         schedules = component_b.site_schedule
         self.add_new_working_day(schedules, working_day)
+        self.validate_working_day(schedules, working_day)
 
+    @classmethod
+    def validate_working_day(self, schedules : List[Shift], shift: Shift):
+        """Valida que el tweak shift este realizado de forma correcta
+        Args:
+            schedules (List[Shift]): _description_
+            shift (Shift): _description_
+        """
+        for i in range(len(schedules)-1):
+            if schedules[i].shift_start == shift.shift_start and schedules[i].shift_end == shift.shift_end:
+                if i>0:
+                    if schedules[i-1].shift_end < schedules[i].shift_end and schedules[i-1].shift_start < schedules[i].shift_start:
+                        pass
+                    else:
+                        raise("error no se relio el tweak de forma correcta")
+                if i < len(schedules)-1:
+                    if schedules[i+1].shift_end > schedules[i].shift_end and schedules[i+1].shift_start > schedules[i].shift_start:
+                        pass
+                    else:
+                        raise("error no se relio el tweak de forma correcta")
+                    
     @classmethod     
     def add_new_working_day(self, schedule: List[Shift], working_day: Shift):
         A = working_day
@@ -28,80 +53,46 @@ class TweakShift:
         assigemend_working = False
         i = 0
         while i < len(schedule)-1:
-            print("i",i)
-            print(f"A: {union.print_working_day(A)}")
-            print(f"B: {union.print_working_day(schedule[i])}")
             left, inner, right = union.calculate(A,schedule[i])
-            print("union: ", left, inner, right)
             if left == [] and inner != [] and right == [] :
-                print("caso 0")
                 pass
             elif left !=[] and inner == [] and right != []:
-                print("caso 0.1")
                 pass
             elif left != [] and inner != [] and right == [] :
-                print("caso 1")
-                "caso 1: todo a esta dentro de b"
                 working = left + inner 
                 schedule[i].shift_start = min(working)
                 schedule[i].shift_end = max(working)
                 assigemend_working = True
                 A = left
-                print(f"A:{A}")
-                print("----------")
             elif left == [] and inner != [] and right != [] and assigemend_working:
-                print("caso 1.1")
-                "caso 1.1: todo B esta en A2 y assigment workig = true"
                 schedule[i].shift_start = min(right)
                 schedule[i].shift_end = max(right)
                 break
             elif left == [] and inner != [] and right != [] and not assigemend_working:
-                print("caso 2")
-                "caso 2: todo B esta en A0 y assigment workig = False" 
                 working = inner + right
                 schedule[i].shift_start = min(working)
                 schedule[i].shift_end = max(right)
                 assigemend_working = True
                 A = right
-                print(f"A:{A}")
-                print("----------")
             elif left == [] and inner != [] and right != [] and assigemend_working:
-                print("caso 2.1")
-                "caso 2.1: todo B esta en A0 y assigment workig = True"
                 schedule[i].shift_start = min(right)
                 schedule[i].shift_end = max(right)
-                print("----------")
                 break
             elif left != [] and inner != [] and right == [] and not assigemend_working:
-                print("caso 3")
-                "caso 3: todo A0 esta dentro de B assigment working = False"
                 schedule[i].shift_start = min(left)
                 schedule[i].shift_end = max(inner)
                 assigemend_working = True
                 A = left
-                print(f"A:{A}")
-                print("----------")
             elif left == [] and inner != [] and right != [] and assigemend_working:
-                print("caso 3.1")
-                "caso 3.1: todo b esta dentro de a2 y assigmet working = True" 
                 schedule[i].shift_start = min(left)
                 schedule[i].shift_end = max(right)
-                print("----------")
                 break
             elif left != [] and inner != [] and right != [] and not assigemend_working:
-                print("caso 4")
-                "caso 4: assigmet working = false" 
-                print("i", i) 
-                # schedule.insert(i,Shift(None,min(right),max(right),None))
                 schedule[i].shift_start = min(right) 
                 schedule[i].shift_end= max(right) 
                 schedule[i+1].shift_start = min(inner) # le suma el los valore faltantes a la siguieten working_day
-                print("i+1",union.print_working_day(schedule[i+1]))
                 assigemend_working = True # lo que esta en left y inner ya se ha asignado
-                print("----------")
             elif left != [] and inner != [] and right != [] and assigemend_working:
-                print("caso 4.1")
-                "caso 4.1: todo A2 esta dentro de B assigment working = True"
                 schedule[i].shift_start = min(right)
                 schedule[i].shift_end= max(right)
                 A = left # lo que esta en left y inner ya se ha asignado tomamos solo lo que esta en left para coparar con la siguiente working day
